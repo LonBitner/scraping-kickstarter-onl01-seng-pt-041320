9cabb3f1eebfa8e72e391bfd79aafe4968b18838
Lon Bitner <lonbitner@gmail.com>
2020-05-24 09:23:38 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,10 +1,7 @@
-
-# require libraries/modules here
 require 'nokogiri'
 require 'open-uri'
 
 def create_project_hash
-  # write your code here
   html= File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
 
